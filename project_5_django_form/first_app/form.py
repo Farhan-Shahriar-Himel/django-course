bf1c42a2e07e24adbd8e6686e4db7040682dc258
Farhan <farhanshim03@gmail.com>
2024-02-24 17:41:11 +0600

    learned django form

--- project_5_django_form/first_app/form.py
+++ project_5_django_form/first_app/form.py
@@ -1,5 +1,13 @@
 from django import forms
 
 class contactFrom(forms.Form):
-    name = forms.CharField()
-    email = forms.EmailField()
+    # name = forms.CharField()
+    # email = forms.EmailField()
+    # age = forms.IntegerField()
+    # weight = forms.FloatField()
+    # balance = forms.DecimalField()
+    # check = forms.BooleanField()
+    # birthday = forms.DateField()
+    # appointment = forms.DateTimeField()
+    size = forms.ChoiceField(choices=[('s', 'small'), ('m', 'medium'), ('l', 'large')])
+    pizza = forms.MultipleChoiceField(choices=[('p', 'papperoni'), ('m', 'mashroom'), ('b', 'beef')])
