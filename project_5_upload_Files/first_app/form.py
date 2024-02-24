417a067c5623a5a0f89db84168f435da33123b36
Farhan <farhanshim03@gmail.com>
2024-02-24 21:35:38 +0600

    learned widget

--- project_5_upload_Files/first_app/form.py
+++ project_5_upload_Files/first_app/form.py
@@ -2,4 +2,8 @@ from django import forms
 
 class contactForm(forms.Form):
     name = forms.CharField()
-    file = forms.FileField()
+    file = forms.FileField(required=False)
+    meal = [('p', 'papperoni'), ('m', 'mashroom'), ('B', 'beef')]
+    pizza = forms.MultipleChoiceField(choices=meal, widget= forms.CheckboxSelectMultiple)
+    birthday = forms.CharField(widget=forms.DateInput(attrs={'type': 'date'}))
+    appointment = forms.CharField(widget=forms.DateInput(attrs={'type': 'datetime-local'}))
